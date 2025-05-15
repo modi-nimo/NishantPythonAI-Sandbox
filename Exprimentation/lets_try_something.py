357e2ab8d6c033445770b632688c40af067816b8
Nishant Modi <modinishant90@gmail.com>
2025-05-15 12:46:29 +0530

    Enhance Gradio UI: implement voice command functionality to transcribe input, send to Ollama, and display responses

--- Exprimentation/lets_try_something.py
+++ Exprimentation/lets_try_something.py
@@ -1,13 +1,4 @@
-# url = "https://hooks.slack.com/triggers/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"
-# import requests
-#
-# body = {
-#   "message": "Example text",
-#   "cell_no": "3"
-# }
-#
-# result = requests.post(url, json=body)
-# print(result.status_code)
+
 """
 - Scrape PDF to be done
 - Retrive complete Section from both documents
