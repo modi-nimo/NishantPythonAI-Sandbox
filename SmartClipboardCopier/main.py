27bae17e2625ca1b3e5c4970adab0ac0c16ce3af
Nishant Modi <modinishant90@gmail.com>
2025-09-09 18:48:51 +0530

    Add clipboard logger to track copied data

---
+++ SmartClipboardCopier/main.py
@@ -0,0 +1,33 @@
+# import apprise
+# from apprise import NotifyType
+#
+# apobj = apprise.Apprise()
+# apobj.add("macosx://")
+# apobj.notify(
+#     body="Testing",
+#     title="Hello World",
+#     notify_type=NotifyType.WARNING
+# )
+import sys
+# ========== Clipboard Logger ==========
+
+import time
+import pyperclip
+from loguru import logger
+
+history = []
+
+logger.add("copied_data_{time}.log", format="{time} {level} {message}", colorize=True)
+def clipboard_logger():
+    recent_value = ""
+    while True:
+        tmp_value = pyperclip.paste()
+        if tmp_value != recent_value:
+            recent_value = tmp_value
+            history.append(recent_value)
+            logger.info(f"Copied: {recent_value}")
+        time.sleep(1)
+
+clipboard_logger()
+
+
